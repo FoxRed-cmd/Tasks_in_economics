9fb0c874c831ee130464f2959b219e644c284ec9
Kirill <blinovk955@gmail.com>
2020-12-13 11:40:19 +0300

    Task 2 and 3

--- ClassD/Class1.cs
+++ ClassD/Class1.cs
@@ -14,4 +14,28 @@ namespace ClassD
 			return (Sum * (Pow(percent, 2) + percent + 1)) / Pow(percent, 3);
 		}
 	}
+	public class Task2_Ec
+	{
+		public double First_transaction { get; set; }
+		public double Second_transaction { get; set; }
+		private double D;
+
+		public double Decision()
+		{
+			D = Pow(145, 2) - 4 * (1000 - First_transaction - Second_transaction) * 10;
+			return (-145 + Sqrt(D)) / 2;
+		}
+	}
+	public class Task3_Ec
+	{
+		public double Sum { get; set; }
+		public double Percent { get; set; }
+		private double temp;
+
+		public double Decision()
+		{
+			temp = Sum * (9 / 1.15);
+			return (10.35 / 9) * temp;
+		}
+	}
 }
--- Tasks_in_economics/Form1.cs
+++ Tasks_in_economics/Form1.cs
@@ -55,6 +55,73 @@ namespace Tasks_in_economics
 					}
 				}
 			};
+			radioButton2.CheckedChanged += (a, e) =>
+			{
+				if (radioButton2.Checked)
+				{
+					textBox1.Text = "31 декабря 2014 года Арсений взял в банке 1 млн. рублей в кредит. Схема выплаты кредита следующая: " +
+					"31 декабря каждого следующего года банк начисляет проценты на оставшуюся сумму долга" +
+					"(то есть увеличивает долг на определённое количество процентов), затем Арсений переводит очередной транш." +
+					"Арсентий выплатил кредит за два транша, переводя в первый раз 550 тыс.рублей, во второй – 638,4 тыс.рублей.Под какой процент банк выдал кредит Арсению ? ";
+
+					label1.Text = "Первый перевод";
+					label2.Text = "Второй перевод";
+
+					foreach (Label item in labels)
+					{
+						item.Visible = true;
+					}
+					foreach (TextBox item in textBoxes)
+					{
+						item.Visible = true;
+					}
+				}
+				else
+				{
+					foreach (Label item in labels)
+					{
+						item.Visible = false;
+					}
+					foreach (TextBox item in textBoxes)
+					{
+						item.Visible = false;
+					}
+				}
+			};
+			radioButton3.CheckedChanged += (a, e) =>
+			{
+				if (radioButton3.Checked)
+				{
+					textBox1.Text = "15 января планируется взять кредит в банке на 9 месяцев. Условия его возврата таковы:" +
+					"- 1 - го числа каждого месяца долг возрастает на 3 % по сравнению с концом предыдущего месяца;" +
+					"-со 2 - го по 14 - е число каждого месяца необходимо выплатить часть долга;" +
+					"-15 - го числа каждого месяца долг должен быть на одну и ту же величину меньше долга на 15 - е число предыдущего месяца." +
+					"Известно, что на пятый месяц кредитования нужно выплатить 57,5 тыс.рублей.Какую сумму нужно вернуть банку в течение всего срока кредитования?";
+
+					label1.Text = "Сумма";
+					label2.Text = "Процент";
+
+					foreach (Label item in labels)
+					{
+						item.Visible = true;
+					}
+					foreach (TextBox item in textBoxes)
+					{
+						item.Visible = true;
+					}
+				}
+				else
+				{
+					foreach (Label item in labels)
+					{
+						item.Visible = false;
+					}
+					foreach (TextBox item in textBoxes)
+					{
+						item.Visible = false;
+					}
+				}
+			};
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
@@ -72,6 +139,24 @@ namespace Tasks_in_economics
 					Percent = Convert.ToDouble(textBox3.Text)
 				};
 				textBox1.Text = "Ответ: " + task1_Ec.Decision() + " рублей";
+			}
+			if (radioButton2.Checked)
+			{
+				Task2_Ec task2_Ec = new Task2_Ec()
+				{
+					First_transaction = Convert.ToDouble(textBox2.Text),
+					Second_transaction = Convert.ToDouble(textBox3.Text)
+				};
+				textBox1.Text = "Ответ: " + task2_Ec.Decision() + " процентов";
+			}
+			if (radioButton3.Checked)
+			{
+				Task3_Ec task3_Ec = new Task3_Ec()
+				{
+					Sum = Convert.ToDouble(textBox2.Text),
+					Percent = Convert.ToDouble(textBox3.Text)
+				};
+				textBox1.Text = textBox1.Text = "Ответ: " + task3_Ec.Decision() + " тыс. рублей";
 			}
 		}
 	}
